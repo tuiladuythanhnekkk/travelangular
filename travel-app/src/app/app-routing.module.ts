6a41515f4bf563f872e72a18202bb51cb39e3d7d
Song Ngan <ngannst20406c@st.uel.edu.vn>
2023-04-17 16:36:50 +0700

    welcome và header nav

--- travel-app/src/app/app-routing.module.ts
+++ travel-app/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './screens/login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { SignupComponent } from './screens/signup/signup.component';
+import { SupportComponent } from './screens/support/support.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homepage', component: HomeComponent
+  },
+
+  {
+    path: 'support', component: SupportComponent
+  },
+
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'signup', component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- travel-app/src/app/app.module.ts
+++ travel-app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PostComponent } from './components/post/post.component';
+import { LoginComponent } from './screens/login/login.component';
+import { SignupComponent } from './screens/signup/signup.component';
+import { SupportComponent } from './screens/support/support.component';
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostComponent,
+    LoginComponent,
+    SignupComponent,
+    SupportComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
