0e3879d72d810f67010292763ae2b20577d74195
Song Ngan <ngannst20406c@st.uel.edu.vn>
2023-04-27 07:07:53 +0700

    search box và icon brand

--- travel-app/src/app/app.module.ts
+++ travel-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -39,6 +39,9 @@ import { NhatrangComponent } from './screens/destination/nhatrang/nhatrang.compo
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
